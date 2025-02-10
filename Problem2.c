2a9fad5635ca0a298c3822e5f277f5e246b2c6e3
tanvir.inc <108387467+tanvir-inc@users.noreply.github.com>
2025-02-10 22:45:23 +0600

    Calculate area of a rectangle by length & width

---
+++ Problem2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int l,w,area;
+
+    printf("Enter length:");
+    scanf("%d",&l);
+    printf("Enter width:");
+    scanf("%d",&w);
+
+    ans= l*w;
+    printf("The area is: %d",area);
+
+    return 0;
+}
