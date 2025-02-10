27a34334a4959b618dc0861e8b505c0bdb9b7297
tanvir.inc <108387467+tanvir-inc@users.noreply.github.com>
2025-02-10 23:01:21 +0600

    Calculate simple interest by principal, rate & time

---
+++ Problem5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int p,r,t,interest;
+
+    printf("Enter principal:");
+    scanf("%d",&r);
+  
+    printf("Enter rate:");
+    scanf("%f",&h);
+  
+    printf("Enter time:");
+    scanf("%d",&t)
+
+    interest= (p*r*t)/100;
+    printf("The interest is %d",interest);
+
+    return 0;
+}
